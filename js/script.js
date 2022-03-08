ce7630ba8ccc54b50e65787f78617d462e852253
truong1609abc <76605779+truong1609abc@users.noreply.github.com>
2022-03-08 18:48:00 +0700

    8/3/2022

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@ $(document).ready(function () {
 
     $('.list-item-slider').slick({
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
         infinite: true,
-        arrows: true,
-        draggable: false,
-        prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
-        nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
         dots: true,
         responsive: [
             {
